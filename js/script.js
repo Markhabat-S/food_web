d002d20f895fbedd3d327727e3dd4b85e9ab39d8
Markhabat <msansyzbay@gmail.com>
2022-02-17 13:30:51 +0600

    добавила rest оператор в класс

--- js/script.js
+++ js/script.js
@@ -155,12 +155,13 @@ window.addEventListener("DOMContentLoaded", () => {
     // использование классов для карточек
 
     class Card {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.transfer = 429;
             this.changeToTenge();
             this.parent = document.querySelector(parentSelector);
@@ -173,8 +174,14 @@ window.addEventListener("DOMContentLoaded", () => {
         render(){
             const element = document.createElement('div');
 
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -183,7 +190,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> тг/день</div>
                 </div>
-            </div>`;
+            `;
 
             this.parent.append(element);
         }
